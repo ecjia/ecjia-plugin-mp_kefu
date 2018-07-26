57de26df05423ee7bd6532cfa970591527bf77f1
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-26 17:55:03 +0800

    重写多客服转发插件

--- mp_kefu.class.php
+++ mp_kefu.class.php
@@ -105,32 +105,71 @@ class mp_kefu extends PlatformAbstract
      * @see \Ecjia\App\Platform\Plugin\PlatformAbstract::eventReply()
      */
     public function eventReply() {
-    	$wechatuser_db = RC_Loader::load_app_model('wechat_user_model','wechat');
-    	$wechat_custom_message_db = RC_Loader::load_app_model('wechat_custom_message_model', 'wechat');
-    	$platform_config_db = RC_Loader::load_app_model('platform_config_model','platform');
-    	RC_Loader::load_app_class('platform_account', 'platform', false);
-    	RC_Loader::load_app_class('wechat_method', 'wechat', false);
+
+        $kefu_status = $this->getConfig('kefu_status');
+        if (empty($kefu_status)) {
+            return null;
+        }
+
+        $kefu_value = $this->getConfig('kefu_value');
+
+        $wechatUUID = new \Ecjia\App\Wechat\WechatUUID();
+
+        $wechat_id = $wechatUUID->getWechatID();
+        $wechat = $wechatUUID->getWechatInstance();
+        $openid = $this->getMessage()->get('FromUserName');
+
+
+        $articles = [
+            'Title'         => '欢迎进入客服系统',
+            'Description'   => '',
+            'Url'           => '',
+            'PicUrl'        => '',
+//            'PicUrl'      => RC_Plugin::plugin_dir_url(__FILE__) . '/images/wechat_thumb_pic.jpg',
+        ];
+        with(new Ecjia\App\Wechat\Sends\SendCustomMessage($wechat, $wechat_id, $openid))->sendNewsMessage($articles['Title'], $articles['Description'], $articles['Url'], $articles['PicUrl']);
+
+        $transfer = new \Royalcms\Component\WeChat\Message\Transfer();
+
+        if ($kefu_value) {
+            $transfer->account($kefu_value);// 或者 $transfer->to($account);
+        }
+
+        return $transfer;
+
+
+
+
+
+
+
+
+//    	$wechatuser_db = RC_Loader::load_app_model('wechat_user_model','wechat');
+//    	$wechat_custom_message_db = RC_Loader::load_app_model('wechat_custom_message_model', 'wechat');
+//    	$platform_config_db = RC_Loader::load_app_model('platform_config_model','platform');
+//    	RC_Loader::load_app_class('platform_account', 'platform', false);
+//    	RC_Loader::load_app_class('wechat_method', 'wechat', false);
     	 
-//     	$openid = $this->from_username;
-    	$uuid = trim($_GET['uuid']);
-    	$account = platform_account::make($uuid);
-    	$wechat_id = $account->getAccountID();
+////     	$openid = $this->from_username;
+//    	$uuid = trim($_GET['uuid']);
+//    	$account = platform_account::make($uuid);
+//    	$wechat_id = $account->getAccountID();
     	//是否超时
 //     	$timeout = false;
 
 //     	$uid  = $wechatuser_db->where(array('openid' => $openid))->get_field('ect_uid');//获取绑定用户会员id
-    	$ext_config  = $platform_config_db->where(array('account_id' => $wechat_id,'ext_code'=>'mp_kefu'))->get_field('ext_config');
-    	$config = array();
-    	$config = unserialize($ext_config);
+//    	$ext_config  = $platform_config_db->where(array('account_id' => $wechat_id,'ext_code'=>'mp_kefu'))->get_field('ext_config');
+//    	$config = array();
+//    	$config = unserialize($ext_config);
     	
-    	foreach ($config as $k => $v) {
-    		if ($v['name'] == 'kefu_status') {
-    			$kefu_status = $v['value'];
-    		}
-    		if ($v['name'] == 'kefu_value') {
-    			$kefu_value = $v['value'];
-    		}
-    	}
+//    	foreach ($config as $k => $v) {
+//    		if ($v['name'] == 'kefu_status') {
+//    			$kefu_status = $v['value'];
+//    		}
+//    		if ($v['name'] == 'kefu_value') {
+//    			$kefu_value = $v['value'];
+//    		}
+//    	}
     	
 //     	$nobd = "还未绑定，需<a href = '".RC_Uri::url('platform/plugin/show', array('handle' => 'mp_userbind/bind_init', 'openid' => $openid, 'uuid' => $_GET['uuid']))."'>点击此处</a>进行绑定";
 //     	if(empty($uid)) {
@@ -146,46 +185,51 @@ class mp_kefu extends PlatformAbstract
 //     		if($time_list[0]['send_time'] - $time_list[1]['send_time'] > 3600 * 2){
 //     			$timeout = true;
 //     		}
-    		$wechat = wechat_method::wechat_instance($uuid);
-    		$msg = array(
-    			'touser' => $this->from_username,
-    			'msgtype' => 'text',
-    			'text' => array(
-    				'content' => '欢迎进入客服系统'
-    			)
-    		);
-    		$wechat->sendCustomMessage($msg);
-			if($kefu_status == 1) {
-				if (empty($kefu_value)) {
-					$content = array(
-						'ToUserName'  => $this->from_username,
-						'FromUserName'=> $this->to_username,
-						'CreateTime'  => SYS_TIME,
-						'MsgType'     => 'transfer_customer_service',
-					);
-				} else {
-					$content = array(
-						'ToUserName'  => $this->from_username,
-						'FromUserName'=> $this->to_username,
-						'CreateTime'  => SYS_TIME,
-						'MsgType'     => 'transfer_customer_service',
-						'TransInfo'   => array(
-							'KfAccount'=>$kefu_value
-						)
-					);
-				}
-			} else {
-				$content = array(
-					'ToUserName' => $this->from_username,
-					'FromUserName' => $this->to_username,
-					'CreateTime' => SYS_TIME,
-					'MsgType' => 'text',
-					'Content' => '抱歉还未开启多客服转接'
-				);
-			}
-//     	}
-        return $content;
+        
+//    		$wechat = wechat_method::wechat_instance($uuid);
+
+
+//    		$msg = array(
+//    			'touser' => $this->from_username,
+//    			'msgtype' => 'text',
+//    			'text' => array(
+//    				'content' => '欢迎进入客服系统'
+//    			)
+//    		);
+//    		$wechat->sendCustomMessage($msg);
+//			if($kefu_status == 1) {
+//				if (empty($kefu_value)) {
+//					$content = array(
+//						'ToUserName'  => $this->from_username,
+//						'FromUserName'=> $this->to_username,
+//						'CreateTime'  => SYS_TIME,
+//						'MsgType'     => 'transfer_customer_service',
+//					);
+//				} else {
+//					$content = array(
+//						'ToUserName'  => $this->from_username,
+//						'FromUserName'=> $this->to_username,
+//						'CreateTime'  => SYS_TIME,
+//						'MsgType'     => 'transfer_customer_service',
+//						'TransInfo'   => array(
+//							'KfAccount'=>$kefu_value
+//						)
+//					);
+//				}
+//			} else {
+//				$content = array(
+//					'ToUserName' => $this->from_username,
+//					'FromUserName' => $this->to_username,
+//					'CreateTime' => SYS_TIME,
+//					'MsgType' => 'text',
+//					'Content' => '抱歉还未开启多客服转接'
+//				);
+//			}
+////     	}
+//        return $content;
     }
+
+
 }
 
 // end
